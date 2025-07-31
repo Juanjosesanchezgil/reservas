1fbb77c5567085d91fefcfa501167a93ff6e2cef
Juan Sanchez <juanjosesanchezgil@gmail.com>
2025-07-31 11:53:56 +0200

    bases de datos e inicializacion

--- app/v1/core/database.py
+++ app/v1/core/database.py
@@ -1,7 +1,27 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 db_file_name = "database.db"
 db_url = f"sqlite///{db_file_name}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(db_url, connect_args=connect_args)
+
+
+Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def init_db():
+    from app.v1.domains.users import models as users_models
+    from app.v1.domains.bookings import models as bookings_models
+    Base.metadata.create_all(bind=engine)
---
+++ app/v1/init_db.py
@@ -0,0 +1,5 @@
+from app.v1.core.database import init_db
+
+if __name__ == "__main__":
+    init_db()
+    print("Tablas creadas")
